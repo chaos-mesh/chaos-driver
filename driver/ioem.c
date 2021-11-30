2629657a90b4258194476a052b8530e9821aec6d
YangKeao <yangkeao@chunibyo.icu>
2021-11-30 17:26:32 +0800

    set elevator for request_queue
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- driver/ioem.c
+++ driver/ioem.c
@@ -15,75 +15,77 @@ struct ioem_data {
 static int ioem_init_sched(struct request_queue *q, struct elevator_type *e)
 {
     struct ioem_data *data;
-	struct elevator_queue *eq;
+    struct elevator_queue *eq;
 
     eq = elevator_alloc(q, e);
     if (!eq)
-		return -ENOMEM;
+        return -ENOMEM;
 
     data = kzalloc_node(sizeof(*data), GFP_KERNEL, q->node);
     if (!data) {
-		kobject_put(&eq->kobj);
-		return -ENOMEM;
-	}
+        kobject_put(&eq->kobj);
+        return -ENOMEM;
+    }
     eq->elevator_data = data;
 
     spin_lock_init(&data->lock);
-	INIT_LIST_HEAD(&data->only_list);
+    INIT_LIST_HEAD(&data->only_list);
+
+    q->elevator = eq;
 
     return 0;
 }
 
 struct request* ioem_dispatch_request(struct blk_mq_hw_ctx * hctx)
 {
-	struct request_queue *q = hctx->queue;
-	struct ioem_data *data = q->elevator->elevator_data;
+    struct request_queue *q = hctx->queue;
+    struct ioem_data *data = q->elevator->elevator_data;
 
     spin_lock(&data->lock);
     if (!list_empty(&data->only_list)) {
         struct request *rq;
 
-		rq = list_first_entry(&data->only_list, struct request, queuelist);
-		list_del_init(&rq->queuelist);
+        rq = list_first_entry(&data->only_list, struct request, queuelist);
+        list_del_init(&rq->queuelist);
 
-		#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0))
-		atomic_dec(&hctx->elevator_queued);
-		#endif
+        #if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0))
+        atomic_dec(&hctx->elevator_queued);
+        #endif
 
         return rq;
 
     }
-	spin_unlock(&data->lock);
+    spin_unlock(&data->lock);
 
-	return NULL;
+    return NULL;
 }
 
 static void ioem_insert_requests(struct blk_mq_hw_ctx * hctx, struct list_head * list, bool at_head) 
 {
-	struct request_queue *q = hctx->queue;
-	struct ioem_data *data = q->elevator->elevator_data;
+    struct request_queue *q = hctx->queue;
+    struct ioem_data *data = q->elevator->elevator_data;
 
-	spin_lock(&data->lock);
+    spin_lock(&data->lock);
     while (!list_empty(list)) {
-		struct request *rq;
+        struct request *rq;
 
-		rq = list_first_entry(list, struct request, queuelist);
-		list_del_init(&rq->queuelist);
+        rq = list_first_entry(list, struct request, queuelist);
+        list_del_init(&rq->queuelist);
 
-		list_add_tail(&rq->queuelist, &data->only_list);
+        list_add_tail(&rq->queuelist, &data->only_list);
 
-		#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0))
-		atomic_inc(&hctx->elevator_queued);
-		#endif
-	}
-	spin_unlock(&data->lock);
+        #if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0))
+        atomic_inc(&hctx->elevator_queued);
+        #endif
+    }
+    spin_unlock(&data->lock);
 }
 
 static struct elevator_type ioem = {
     .ops = {
         .init_sched = ioem_init_sched,
-		.insert_requests = ioem_insert_requests,
-		.dispatch_request = ioem_dispatch_request,
+        .insert_requests = ioem_insert_requests,
+        .dispatch_request = ioem_dispatch_request,
     },
     .elevator_name = "ioem",
     .elevator_owner = THIS_MODULE,
