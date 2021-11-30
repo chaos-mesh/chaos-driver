77abbc355b9d34cf0b0c8765fa084d472adfa549
YangKeao <yangkeao@chunibyo.icu>
2021-11-30 20:05:46 +0800

    add current time and rbtree scheduler
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- driver/ioem.c
+++ driver/ioem.c
@@ -3,11 +3,25 @@
 #include <linux/elevator.h>
 #include <linux/spinlock.h>
 #include <linux/version.h>
+#include <linux/rbtree.h>
 
 #include "ioem.h"
 
+#define rb_to_rq(rb) rb_entry_safe(rb, struct request, rb_node)
+#define rq_rb_first(root) rb_to_rq(rb_first(root))
+
+struct ioem_priv {
+    u64 time_to_send;
+};
+
+struct ioem_priv* ioem_priv(struct request *rq)
+{
+    // `priv` has two pointers long, is enough to store the `ioem_priv`.
+    return (struct ioem_priv*)(&rq->elv.priv[0]);
+}
+
 struct ioem_data {
-    struct list_head only_list;
+    struct rb_root root;
 
     spinlock_t lock;
 };
@@ -29,23 +43,55 @@ static int ioem_init_sched(struct request_queue *q, struct elevator_type *e)
     eq->elevator_data = data;
 
     spin_lock_init(&data->lock);
-    INIT_LIST_HEAD(&data->only_list);
+    data->root = RB_ROOT;
 
     q->elevator = eq;
 
     return 0;
 }
 
+static void ioem_enqueue(struct ioem_data *data, struct request *rq)
+{
+    struct rb_node **p = &data->root.rb_node, *parent = NULL;
+
+    while (*p) {
+        struct request* parent_rq;
+
+        parent = *p;
+        parent_rq = rb_entry_safe(parent, struct request, rb_node);
+
+        if (ioem_priv(rq)->time_to_send >= ioem_priv(parent_rq)->time_to_send)
+            p = &parent->rb_right;
+        else
+            p = &parent->rb_left;
+    }
+
+    rb_link_node(&rq->rb_node, parent, p);
+    rb_insert_color(&rq->rb_node, &data->root);
+}
+
+static struct request* ioem_peek_request(struct ioem_data *data)
+{
+    struct request* ioem_rq = rq_rb_first(&data->root);
+
+    return ioem_rq;
+}
+
+static void ioem_erase_head(struct ioem_data *data, struct request *rq)
+{
+    rb_erase(&rq->rb_node, &data->root);
+}
+
 struct request* ioem_dispatch_request(struct blk_mq_hw_ctx * hctx)
 {
-	struct request *rq = NULL;
+    struct request *rq = NULL;
     struct request_queue *q = hctx->queue;
     struct ioem_data *data = q->elevator->elevator_data;
 
     spin_lock(&data->lock);
-    if (!list_empty(&data->only_list)) {
-        rq = list_first_entry(&data->only_list, struct request, queuelist);
-        list_del_init(&rq->queuelist);
+    if (!RB_EMPTY_ROOT(&data->root)) {
+        rq = ioem_peek_request(data);
+        ioem_erase_head(data, rq);
 
         #if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0))
         atomic_dec(&hctx->elevator_queued);
@@ -68,7 +114,8 @@ static void ioem_insert_requests(struct blk_mq_hw_ctx * hctx, struct list_head *
         rq = list_first_entry(list, struct request, queuelist);
         list_del_init(&rq->queuelist);
 
-        list_add_tail(&rq->queuelist, &data->only_list);
+        ioem_priv(rq)->time_to_send = ktime_get_ns();
+        ioem_enqueue(data, rq);
 
         #if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0))
         atomic_inc(&hctx->elevator_queued);
@@ -81,13 +128,13 @@ static bool has_work(struct blk_mq_hw_ctx * hctx)
 {
     struct request_queue *q = hctx->queue;
     struct ioem_data *data = q->elevator->elevator_data;
-	bool has_work = 0;
+    bool has_work = 0;
 
-	spin_lock(&data->lock);
-	has_work = !list_empty(&data->only_list);
-	spin_unlock(&data->lock);
+    spin_lock(&data->lock);
+    has_work = !RB_EMPTY_ROOT(&data->root);
+    spin_unlock(&data->lock);
 
-	return has_work;
+    return has_work;
 }
 
 static struct elevator_type ioem = {
@@ -95,7 +142,7 @@ static struct elevator_type ioem = {
         .init_sched = ioem_init_sched,
         .insert_requests = ioem_insert_requests,
         .dispatch_request = ioem_dispatch_request,
-		.has_work = has_work,
+        .has_work = has_work,
     },
     .elevator_name = "ioem",
     .elevator_owner = THIS_MODULE,
